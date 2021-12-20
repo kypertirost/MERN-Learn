4f4ca7de8594a1c82a443f98aba5a2041495514f
Miao Liu <kyper.liu1018@gmail.com>
2021-12-19 23:10:13 -0500

    add front end

---
+++ frontend/src/components/restaurant.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Restaurant() {
+  return (
+    <div className="App">
+      Hello World.
+    </div>
+  );
+}
+
+export default Restaurant;
---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RestaurantsList() {
+  return (
+    <div className="App">
+      Hello World.
+    </div>
+  );
+}
+
+export default RestaurantsList;
